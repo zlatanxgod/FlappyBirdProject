352eb00dc6d917df49e5248f2a27e155b179616b
zlatanxgod <abhishantsingh05@gmail.com>
2022-03-27 17:38:20 +0530

    change for multiple asset

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ def draw_window(win,bird,pipes,base, score):
     base.draw(win)
     pygame.display.update()
 
-def main():
+def main(genomes, config):
     bird = Bird(200,200)
     base = Base(650)
     pipes = [Pipe(600)]
@@ -184,6 +184,24 @@ def main():
     quit()
 
 main()
+
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    p = neat.population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main,50)
+
+
+if __name__ == "__main__":
+    local_dir=os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+    run(config_path)
 
 
 
